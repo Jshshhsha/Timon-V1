656b1ccb68c17c0be1fdf11424f1f2f4bcae3e8a
Jshshhsha <113236041+Jshshhsha@users.noreply.github.com>
2023-04-22 19:43:53 +0400

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -39,7 +39,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 }
 handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
 handler.tags = ['sticker']
-handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
+handler.command = /^(sticker|s|ملصق)$/i
 
 export default handler
 
