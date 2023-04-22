3728f8c8c55960b44dcb8763d1b600fe5cb9f2e5
Jshshhsha <113236041+Jshshhsha@users.noreply.github.com>
2023-04-22 19:13:07 +0400

    Update group-link.js

--- plugins/group-link.js
+++ plugins/group-link.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, args }) => {
 }
 handler.help = ['linkgroup']
 handler.tags = ['group']
-handler.command = /^link(gro?up)?$/i
+handler.command = /^(رابط.جروب)$/i
 
 
 export default handler
