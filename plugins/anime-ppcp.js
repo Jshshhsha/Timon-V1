8e0ce4864f5619e36e23719ef535ada7e3f07115
Jshshhsha <113236041+Jshshhsha@users.noreply.github.com>
2023-04-22 13:25:10 +0400

    Update anime-ppcp.js

--- plugins/anime-ppcp.js
+++ plugins/anime-ppcp.js
@@ -2,10 +2,10 @@ import { Couples } from 'dhn-api'
 var handler = async (m, { conn, command }) => {
 
 const res = await Couples() 
-await conn.sendFile(m.chat, res.male, 'ppcp.jpg', 'Male', m) 
-return conn.sendFile(m.chat, res.female, 'ppcp.jpg', 'Female', m)
+await conn.sendFile(m.chat, res.male, 'ppcp.jpg', '( ذكـر ) (🙋🏻‍♂️)', m) 
+return conn.sendFile(m.chat, res.female, 'ppcp.jpg', '( أنـثـى ) (🙋🏻‍♀️)', m)
 }
 handler.help = ['ppcouple']
 handler.tags = ['internet']
-handler.command = /^(ppcp|ppcouple)$/i
+handler.command = /^(تطقيم|طقم)$/i
 export default handler
