7f2ae22c2206b382fd02d70dd00b6c729f24aa06
Jshshhsha <113236041+Jshshhsha@users.noreply.github.com>
2023-04-22 12:23:01 +0400

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`${text ? `${text}\n` : ''}┌─「 Tag All 」\n` + users.map(v => '│🇵🇰 @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, {
+    m.reply(`${text ? `${text}\n` : ''}┌─「 الـمـنـشـن الـجـمـاعـي 」\n` + users.map(v => '│🐈‍⬛ : @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, {
         mentions: users
     })
 }
 
 handler.help = ['tagall']
 handler.tags = ['group']
-handler.command = ['tagall']
+handler.command = ['منشن.جماعي|منشن']
 handler.admin = true
 handler.group = true
 
