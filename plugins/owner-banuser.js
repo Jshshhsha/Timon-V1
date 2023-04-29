8453540a31a4bc919f12dfad23d99c940847457c
Jshshhsha <113236041+Jshshhsha@users.noreply.github.com>
2023-04-29 19:48:16 +0400

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,14 +1,14 @@
 // import db from '../lib/database.js'
 
 let handler = async (m, { conn, text }) => {
-    if (!text) throw 'Who wants to be banned??🗿'
+    if (!text) throw '( اكتب الرقم الذي ترغب ب حظره )'
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
     if (!who) throw 'Tag one of them bro'
     let users = db.data.users
     users[who].banned = true
-    conn.reply(m.chat, `Mampos banned awowkwkowkw`, m)
+    conn.reply(m.chat, `( تم الحظر )`, m)
 }
 handler.help = ['ban @user']
 handler.tags = ['owner']
