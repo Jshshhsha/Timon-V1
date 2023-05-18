0cae2587d4a9c534b331153c426c74779440f6d5
Mikasa665 <133528192+Mikasa665@users.noreply.github.com>
2023-05-18 21:36:06 +0300

    Update sticker-emojimix.js

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -4,7 +4,7 @@ import { sticker } from '../lib/sticker.js'
 import fs from 'fs'
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-  if (!args[0]) throw `*⛌ Enter the Emoji you want to combine*\n\n*• Example:*\n- ${usedPrefix + command} 🐱+👻\n\n[ minimum 2 emojis ]`
+  if (!args[0]) throw `*⛌ Enter the Emoji you want to combine*\n\n*• مثال:*\n- ${usedPrefix + command} 🐱+👻\n\n[ minimum 2 emojis ]`
   let [emoji1, emoji2] = text.split`+`
   const anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)).json()
   if (anu.results[0] == undefined) throw 'Kombinasi Emojimix Tidak Ditemukan'
@@ -15,6 +15,6 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
 handler.help = ['emojimix']
 handler.tags = ['sticker']
-handler.command = /^(emojimix|emix)$/i
+handler.command = /^(دمج|emix)$/i
 
 export default handler
